3eca5d22bb1cf7a7e5590e8ab072dc390e4088ec
LenaCScheper <ven.lenysja@ukr.net>
2022-02-19 16:16:06 +0200

    скопировала данные

---
+++ build/precache-manifest.56262548ea397703bccf54aad752b7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c8478aea163fc7a67a52e069c1fb4f",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "f410ac9cc938c073dd67",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.6484bb7c.chunk.css"
+  },
+  {
+    "revision": "f4151cca25f84c0fd69f",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.14b0ad3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.14b0ad3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f410ac9cc938c073dd67",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.97fb4f97.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
